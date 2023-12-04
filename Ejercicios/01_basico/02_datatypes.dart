f8686908fc452e1251d4b6af74843ee324ac0466
Alejandro Gimeno Martin <alejandro@xanasystem.com>
2023-12-04 21:11:06 +0100

    Tipado de números

---
+++ Ejercicios/01_basico/02_datatypes.dart
@@ -0,0 +1,13 @@
+main() {
+
+  // ======= Números
+  var a = 10; // var--> Variable comodin para que se asigne automaticamente el tipo
+  dynamic b = 10; // dynamic --> Variable comodin para que se asigne automaticamente el tipo
+  int c = 10; // int --> Variable numerica
+  double d = 10.0; // double --> Variable decimal
+  int? e =null; // ? --> Permite introducir un null
+  
+
+  print(a);
+
+}
