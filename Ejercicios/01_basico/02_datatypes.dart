7aec91bd84ec11415e8a1e524fd050a135c1a82b
Alejandro Gimeno Martin <alejandro@xanasystem.com>
2023-12-04 21:19:46 +0100

    Tipado de strings

--- Ejercicios/01_basico/02_datatypes.dart
+++ Ejercicios/01_basico/02_datatypes.dart
@@ -6,8 +6,23 @@ main() {
   int c = 10; // int --> Variable numerica
   double d = 10.0; // double --> Variable decimal
   int? e =null; // ? --> Permite introducir un null
-  
+
 
   print(a);
+
+  // ======= String
+  var nombre = 'Alejandro';
+  String apellido = 'Gimeno';
+  String nombreCompleto = '$nombre $apellido';
+  String multilinea = '''
+    Hola mundo.
+    ¿Como estas?
+    Puedo poner una "'" sin escapar.
+    $nombreCompleto con el \$ añadimos una variable dentro de este string
+  ''';
+
+
+
+  print( nombre );
 
 }
